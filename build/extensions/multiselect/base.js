bcfabe7272c278b50a0aa932630adce0d1a862b3
zengyue <yezengyue@gmail.com>
2014-06-26 13:45:45 +0800

    build project

--- build/extensions/multiselect/base.js
+++ build/extensions/multiselect/base.js
@@ -1,11 +1,1 @@
-/**
- * @fileOverview 选择框命名空间入口文件
- * @ignore
- */
-
-define('bui/extensions/multiselect', ['bui/common', 'bui/extensions/multiselect/multilist', 'bui/extensions/multiselect/multilistpicker','bui/extensions/multiselect/multiselect', 'bui/extensions/search'], function (require) {
-
-  var BUI = require('bui/common'),
-    multiselect = require('bui/extensions/multiselect/multiselect');
-  return multiselect;
-});
+define("bui/extensions/multiselect",["bui/common","bui/extensions/multiselect/multilist","bui/extensions/multiselect/multilistpicker","bui/extensions/multiselect/multiselect","bui/extensions/search"],function(require){var e=(require("bui/common"),require("bui/extensions/multiselect/multiselect"));return e});

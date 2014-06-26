d74d709f52e6b8a6b9f6c94fcaf75c4bb5b67b98
zengyue <yezengyue@gmail.com>
2014-06-26 15:05:38 +0800

    slider 添加common的依赖

--- build/slider-debug.js
+++ build/slider-debug.js
@@ -1,12 +1,15 @@
-define('bui/slider',['bui/slider/slider'],function (require) {
-  var Slider = BUI.namespace('Slider');
+define('bui/slider',['bui/common', 'bui/slider/slider'],function (require) {
+
+  var BUI = require('bui/common'), 
+    Slider = BUI.namespace('Slider');
 
   BUI.mix(Slider,{
-    Slider : require('bui/slider/slider')
+    Slider: require('bui/slider/slider')
   });
 
   return Slider;
-});/**
+});
+/**
  * @fileOverview 滑块
  * @ignore
  */
